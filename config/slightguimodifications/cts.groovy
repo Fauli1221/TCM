392d0c5bbc05c66396f6aa68c000fe2a9a3df717
Fauli1221 <paul.schugardt@gmail.com>
2021-09-14 18:45:33 +0200

    added config and shader

---
+++ config/slightguimodifications/cts.groovy
@@ -0,0 +1,15 @@
+mainMenu {
+    enabled = true
+    label {
+        position {
+            x { it - 2 }
+            y { it - 20 }
+        }
+        text = literal("Fauli's Techtastisch Community Modpack V1.3-1.17.1")
+        align = "right"
+        color = 0xFFFFFF
+        hoveredColor = 0x55FFFF
+        shadow = true
+        onClicked = url("https://twitter.com/fauli1221")
+    }
+}
