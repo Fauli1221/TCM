73d7c20741ef6c5ec6bc7b8683f9a5cfa9828aa9
Fauli1221 <paul.schugardt@gmail.com>
2021-11-01 20:28:53 +0100

    updated settings

--- config/slightguimodifications/cts.groovy
+++ config/slightguimodifications/cts.groovy
@@ -5,11 +5,11 @@ mainMenu {
             x { it - 2 }
             y { it - 20 }
         }
-        text = literal("Fauli's Techtastisch Community Modpack Nightmare Night Upgrade V1.5-1.17.1")
+        text = literal("Fauli's Techtastisch Community Modpack Back to Normal Upgrade V1.6-1.17.1")
         align = "right"
         color = 0xFFFFFF
         hoveredColor = 0x55FFFF
         shadow = true
-        onClicked = url("https://twitter.com/fauli1221")
+        onClicked = url("https://github.com/Fauli1221/TCM")
     }
 }
