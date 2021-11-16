ada30d016d535143ae9fa53ef0da75af5f2e86ba
Fauli1221 <paul.schugardt@gmail.com>
2021-11-16 20:39:53 +0100

    updated version string

--- config/slightguimodifications/cts.groovy
+++ config/slightguimodifications/cts.groovy
@@ -5,7 +5,7 @@ mainMenu {
             x { it - 2 }
             y { it - 20 }
         }
-        text = literal("Fauli's Techtastisch Community Modpack Back to Normal Upgrade V1.6-1.17.1")
+        text = literal("Fauli's Techtastisch Community Modpack V1.6.1-1.17.1")
         align = "right"
         color = 0xFFFFFF
         hoveredColor = 0x55FFFF
